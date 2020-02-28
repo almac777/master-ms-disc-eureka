09d9a3b388bf2d0b8a7ff27d46db4c480a53b312
Alex Macovei <Alex.Macovei@twinformatics.at>
2020-02-28 14:33:21 +0100

    Init commit

---
+++ src/test/java/at/ac/fhcampus/master/micro/eureka/microeureka/MicroEurekaApplicationTests.java
@@ -0,0 +1,12 @@
+package at.ac.fhcampus.master.micro.eureka.microeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
